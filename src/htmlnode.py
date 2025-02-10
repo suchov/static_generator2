e3ec80a43eaa8354ff2ac7a1d18c18614a93a2c8
Artem Sychov <artem@sychov.dev>
2025-02-10 20:57:30 +0100

    add LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -3,8 +3,8 @@ class HTMLNode():
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
-        self.children = children or []
-        self.props = props or {}
+        self.children = children
+        self.props = props
 
     def to_html(self):
         raise NotImplementedError("Subclasses should implement this method.")
@@ -16,3 +16,19 @@ class HTMLNode():
 
     def __repr__(self):
         return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        if value is None:
+            raise ValueError("LeafNode must have a value.")
+
+        super().__init__(tag=tag, value=value, children=None, props=props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode must have a value.")
+
+        if self.tag is None:
+            return self.value
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -14,9 +14,49 @@ class TestHTMLNode(unittest.TestCase):
 
     def test_repr_method(self):
         node = HTMLNode(tag="p", value="Hello World!", props={"class": "text-bold"})
-        expected_repr = "HTMLNode(tag=p, value=Hello World!, children=[], props={'class': 'text-bold'})"
+        expected_repr = "HTMLNode(tag=p, value=Hello World!, children=None, props={'class': 'text-bold'})"
         self.assertEqual(repr(node), expected_repr)
 
+    def test_values(self):
+        node = HTMLNode(
+                "div",
+                "I wish I could read",
+        )
+        self.assertEqual(
+                node.tag,
+                "div",
+        )
+        self.assertEqual(
+                node.value,
+                "I wish I could read",
+        )
+        self.assertEqual(
+                node.children,
+                None,
+        )
+        self.assertEqual(
+                node.props,
+                None,
+        )
+
+
+class TestLeafNode(unittest.TestCase):
+
+    def test_leafnode_renders_plain_text(self):
+        node = LeafNode(None, "Just text")
+        self.assertEqual(node.to_html(), "Just text")
+
+    def test_leafnode_renders_with_tag(self):
+        node = LeafNode("p", "This is a paragraph.")
+        self.assertEqual(node.to_html(), "<p>This is a paragraph.</p>")
+
+    def test_leafnode_renders_with_tag_and_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+    def test_teafnode_without_value_raise_error(self):
+        with self.assertRaises(ValueError):
+            LeafNode("p", None)
 
 if __name__ == "__main__":
     unittest.mian()
