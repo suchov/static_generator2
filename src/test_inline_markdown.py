62e7860a2afaa2737e0b38279d62636c4e4a3a4a
Artem Sychov <artem@sychov.dev>
2025-02-15 13:52:23 +0100

    add links and images extracter

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,5 +1,9 @@
 import unittest
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import (
+        split_nodes_delimiter,
+        extract_markdown_links,
+        extract_markdown_images,
+)
 from textnode import TextNode, TextType
 
 
@@ -78,6 +82,22 @@ class TestInlineMarkdown(unittest.TestCase):
                     TextNode(" word", TextType.TEXT),
                 ],
                 new_nodes,
+        )
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)")
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev)"
+        )
+        self.assertListEqual(
+                [
+                    ("link", "https://boot.dev"),
+                    ("another link", "https://blog.boot.dev"),
+                ],
+                matches,
         )
 
 
